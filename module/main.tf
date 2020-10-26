fdb0c4ac1a3eb320074ff7053e4e1a5fcc1399b3
Vladimir Mukhin <vlad@mukhin.page>
2020-10-25 21:18:02 -0400

    Restructure modules

--- module/main.tf
+++ module/main.tf
@@ -43,223 +43,26 @@ module "vpc" {
   }
 }
 
-#Create security groups
-resource "aws_security_group" "cluster" {
-  name_prefix = var.cluster_name
-  description = "EKS cluster security group."
-  vpc_id      = module.vpc.vpc_id
-  tags = {
-    "Name" = "${var.cluster_name}-eks_cluster_sg"
-  }
-}
+module "eks" {
+  source = "github.com/terraform-aws-modules/terraform-aws-eks?ref=v12.2.0"
 
-resource "aws_security_group" "workers" {
-  name_prefix = var.cluster_name
-  description = "Security group for all nodes in the cluster."
-  vpc_id      = module.vpc.vpc_id
-  tags = {
-    "Name"                                      = "${var.cluster_name}-eks_worker_sg"
-    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
-  }
-}
+  cluster_name = var.cluster_name
+  subnets      = module.vpc.private_subnets
 
-resource "aws_security_group_rule" "cluster_egress_internet" {
-  description       = "Allow cluster egress access to the Internet."
-  protocol          = "-1"
-  security_group_id = aws_security_group.cluster.id
-  cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 0
-  to_port           = 0
-  type              = "egress"
-}
+  vpc_id = module.vpc.vpc_id
 
-resource "aws_security_group_rule" "cluster_https_worker_ingress" {
-  description              = "Allow pods to communicate with the EKS cluster API."
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.cluster.id
-  source_security_group_id = aws_security_group.workers.id
-  from_port                = 443
-  to_port                  = 443
-  type                     = "ingress"
-}
-
-resource "aws_security_group_rule" "workers_egress_internet" {
-  description       = "Allow nodes all egress to the Internet."
-  protocol          = "-1"
-  security_group_id = aws_security_group.workers.id
-  cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 0
-  to_port           = 0
-  type              = "egress"
-}
-
-resource "aws_security_group_rule" "workers_ingress_self" {
-  description              = "Allow node to communicate with each other."
-  protocol                 = "-1"
-  security_group_id        = aws_security_group.workers.id
-  source_security_group_id = aws_security_group.workers.id
-  from_port                = 0
-  to_port                  = 65535
-  type                     = "ingress"
-}
-
-resource "aws_security_group_rule" "workers_ingress_cluster" {
-  description              = "Allow workers pods to receive communication from the cluster control plane."
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.workers.id
-  source_security_group_id = aws_security_group.cluster.id
-  from_port                = 1025
-  to_port                  = 65535
-  type                     = "ingress"
-}
-
-resource "aws_security_group_rule" "workers_ingress_cluster_kubelet" {
-  description              = "Allow workers Kubelets to receive communication from the cluster control plane."
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.workers.id
-  source_security_group_id = aws_security_group.cluster.id
-  from_port                = 10250
-  to_port                  = 10250
-  type                     = "ingress"
-}
-
-resource "aws_security_group_rule" "workers_ingress_cluster_https" {
-  description              = "Allow pods running extension API servers on port 443 to receive communication from cluster control plane."
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.workers.id
-  source_security_group_id = aws_security_group.cluster.id
-  from_port                = 443
-  to_port                  = 443
-  type                     = "ingress"
-}
-
-resource "aws_security_group_rule" "workers_ingress_cluster_primary" {
-  description              = "Allow pods running on workers to receive communication from cluster primary security group (e.g. Fargate pods)."
-  protocol                 = "all"
-  security_group_id        = aws_security_group.workers.id
-  source_security_group_id = aws_security_group.cluster.id
-  from_port                = 0
-  to_port                  = 65535
-  type                     = "ingress"
-}
-
-resource "aws_security_group_rule" "cluster_primary_ingress_workers" {
-  description              = "Allow pods running on workers to send communication to cluster primary security group (e.g. Fargate pods)."
-  protocol                 = "all"
-  security_group_id        = aws_security_group.cluster.id
-  source_security_group_id = aws_security_group.workers.id
-  from_port                = 0
-  to_port                  = 65535
-  type                     = "ingress"
-}
-
-#Create IAM roles
-resource "aws_iam_role" "cluster" {
-  name_prefix           = var.cluster_name
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-data "aws_partition" "current" {}
-
-locals {
-  policy_arn_prefix = "arn:${data.aws_partition.current.partition}:iam::aws:policy"
-}
-
-resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
-  policy_arn = "${local.policy_arn_prefix}/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.cluster.name
-}
-
-resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSServicePolicy" {
-  policy_arn = "${local.policy_arn_prefix}/AmazonEKSServicePolicy"
-  role       = aws_iam_role.cluster.name
-}
-
-resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSVPCResourceControllerPolicy" {
-  policy_arn = "${local.policy_arn_prefix}/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.cluster.name
-}
-
-resource "aws_iam_role" "workers" {
-  name = "${var.cluster_name}-workers"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "workers-AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.workers.name
-}
-
-resource "aws_iam_role_policy_attachment" "workers-AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.workers.name
-}
-
-resource "aws_iam_role_policy_attachment" "workers-AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.workers.name
-}
-
-#Create EKS cluster
-resource "aws_eks_cluster" "this" {
-  name                      = var.cluster_name
-  role_arn                  = aws_iam_role.cluster.arn
-
-  vpc_config {
-    security_group_ids      = compact([aws_security_group.cluster.id])
-    subnet_ids              = module.vpc.private_subnets
-    
-    endpoint_public_access  = true
-    endpoint_private_access = true
-    public_access_cidrs     = ["73.126.49.102/32"]
-  }
-
-  depends_on = [
-    aws_security_group_rule.cluster_egress_internet,
-    aws_security_group_rule.cluster_https_worker_ingress,
-    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.cluster_AmazonEKSServicePolicy,
-    aws_iam_role_policy_attachment.cluster_AmazonEKSVPCResourceControllerPolicy,
-  ]
-}
-
-resource "aws_eks_node_group" "workers" {
-  cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "${var.cluster_name}-workers"
-  node_role_arn   = aws_iam_role.workers.arn
-  subnet_ids      = module.vpc.private_subnets
-
-  scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
-  }
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  depends_on = [
-    aws_iam_role_policy_attachment.workers-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.workers-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.workers-AmazonEC2ContainerRegistryReadOnly,
+  worker_groups = [
+    {
+      name                 = "worker-group-1"
+      instance_type        = "t2.medium"
+      asg_desired_capacity = 3
+      tags = [
+        {
+          "key"                 = "k8s.io/cluster-autoscaler/${var.cluster_name}",
+          "value"               = "owned",
+          "propagate_at_launch" = "true"
+        }
+      ]
+    }
   ]
 }
